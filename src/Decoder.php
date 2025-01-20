e838ec5723b5956eab12c5e4b310c69d65119da7
Alexander Ebert <ebert@woltlab.com>
2025-01-20 15:37:55 +0100

    Port the code to use `nelexa/buffer`

--- src/Decoder.php
+++ src/Decoder.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Woltlab\WebpExif;
 
+use Nelexa\Buffer\Buffer;
+use Nelexa\Buffer\StringBuffer;
 use RuntimeException;
 use TypeError;
 
@@ -11,7 +13,9 @@ final class Decoder
 {
     public function fromBinary(string $binary): WebP
     {
-        $totalSize = \strlen($binary);
+        $buffer = new StringBuffer($binary);
+        $buffer->setOrder(Buffer::LITTLE_ENDIAN);
+        $buffer->setReadOnly(true);
 
         // A RIFF container at its minimum contains the "RIFF" header, a
         // uint32LE representing the chunk size, the "WEBP" type and the data
@@ -21,33 +25,31 @@ final class Decoder
         // The shortest possible WebP image is a simple VP8L container that
         // contains only the magic byte, a uint32 for the flags and dimensions,
         // and at last a single byte of data. This takes up 26 bytes in total.
-        if ($totalSize < 26) {
+        if ($buffer->size() < 26) {
             throw new RuntimeException("TODO: too small");
         }
 
-        $header = \unpack('A4riff/Vlength/A4format', $binary);
-        \assert($header !== false);
-        if ($header['riff'] !== 'RIFF' || $header['format'] !== 'WEBP') {
+        $riff = $buffer->getString(4);
+        $length = $buffer->getUnsignedInt();
+        $format = $buffer->getString(4);
+        if ($riff !== 'RIFF' || $format !== 'WEBP') {
             throw new RuntimeException("TODO: not riff/webp");
         }
 
         // The length in the header is does not include "RIFF" and the length
         // itself. It must therefore be exactly 8 bytes shorter than the total
         // size.
-        if ($header['length'] !== $totalSize - 8) {
+        if ($length !== $buffer->size() - 8) {
             throw new RuntimeException("TODO: length mismatch");
         }
 
-        $offset = 12;
-        $topChunk = $this->decodeChunk($binary, $offset);
-        $offset += 8;
-
-        switch ($topChunk->type) {
+        $fourCC = $buffer->getString(4);
+        switch (ChunkType::fromFourCC($fourCC)) {
             case ChunkType::VP8:
-                return $this->decodeLossy($binary, $offset, $topChunk);
+                return $this->decodeLossy($buffer);
 
             case ChunkType::VP8L:
-                return $this->decodeLossless($binary, $offset, $topChunk);
+                return $this->decodeLossless($buffer);
 
             case ChunkType::VP8X:
                 // We're implicitly discarding the top chunk here because it
@@ -55,11 +57,11 @@ final class Decoder
                 // `$binary` and using the `$topChunk` means we have to reset
                 // the offset. Preserving the existing offset means we can
                 // output meaningful offsets in error messages if we need to.
-                return $this->decodeExtendedHeader($binary, $offset);
+                return $this->decodeExtendedHeader($buffer);
 
             default: {
-                    $originalOffset = $offset - 8;
-                    throw new RuntimeException("TODO: unexpected chunk {$topChunk->fourCC} at offset {$originalOffset}");
+                    $originalOffset = $buffer->position() - 4;
+                    throw new RuntimeException("TODO: unexpected chunk {$fourCC} at offset {$originalOffset}");
                 }
         }
     }
@@ -67,105 +69,108 @@ final class Decoder
     /**
      * @see https://datatracker.ietf.org/doc/html/rfc6386
      */
-    private function decodeLossy(string $binary, int $offset, Chunk $topChunk): WebP
+    private function decodeLossy(Buffer $buffer): WebP
     {
-        $totalSize = \strlen($binary);
+        $length = $buffer->getUnsignedInt();
+        $startOfData = $buffer->position();
+        //$totalSize = \strlen($binary);
 
-        $header = \unpack('ctag', $binary, $offset);
-        \assert($header !== false);
-
-        $offset += 1;
+        $tag = $buffer->getUnsignedByte();
 
         // We expect the first frame to be a keyframe.
-        $frameType = $header['tag'] & 1;
+        $frameType = $tag & 1;
         if ($frameType !== 0) {
             throw new RuntimeException("Expected the first frame to be a keyframe");
         }
 
         // Skip the next two bytes, they are part of the header but do not
         // contain any information that is relevant to us.
-        $offset += 2;
+        $buffer->skip(2);
 
         // Keyframes must start with 3 magic bytes.
-        if ($binary[$offset] !== "\x9D" || $binary[$offset + 1] !== "\x01" || $binary[$offset + 2] !== "\x2A") {
+        $marker = $buffer->getString(3);
+        if ($marker !== "\x9D\x01\x2A") {
             throw new RuntimeException("Expected the magic bytes 0x9D 0x01 0x2A at the start of the keyframe");
         }
 
-        $offset += 3;
-
         // The width and height are encoded using 2 bytes each. However, the
         // first two bits are the scale followed by 14 bits for the dimension.
-        $dimensions = \unpack('vwidth/vheight', $binary, $offset);
-        \assert($dimensions !== false);
+        $width = $buffer->getUnsignedShort() & 0x3FFF;
+        $height = $buffer->getUnsignedShort() & 0x3FFF;
 
-        $width = $dimensions['width'] & 0x3FFF;
-        $height = $dimensions['height'] & 0x3FFF;
-
-        return WebP::fromChunks($width, $height, [$topChunk]);
+        return WebP::fromChunks(
+            $width,
+            $height,
+            [
+                new Chunk("VP8 ", $buffer->setPosition($startOfData)->getString($length))
+            ]
+        );
     }
 
-    private function decodeLossless(string $binary, int $offset, Chunk $topChunk): WebP
+    private function decodeLossless(Buffer $buffer): WebP
     {
-        if ($binary[$offset] !== "\x2F") {
+        $length = $buffer->getUnsignedInt();
+        $startOfData = $buffer->position();
+
+        $signature = $buffer->getUnsignedByte();
+        if ($signature !== 0x2F) {
             throw new RuntimeException("TODO: invalid signature for lossless");
         }
 
-        $offset += 1;
-
-        $header = \unpack("Vheader", $binary, $offset);
-        \assert($header !== false);
+        $header = $buffer->getUnsignedInt();
 
         // The header contains the following data:
         // 0-13: width - 1
         // 14-27: height - 1
         // 28: alpha_is_used
         // 29-31: version (must be 0)
-        $header = $header['header'];
         $version = $header >> 29;
         if ($version !== 0) {
             throw new RuntimeException("Expected the version to be 0, found {$version} instead");
         }
 
-        $width = (1 + $header) & 0x3FFF;
-        $height = (1 + ($header >> 14)) & 0x3FFF;
+        $width = ($header & 0x3FFF) + 1;
+        $height = (($header >> 14) & 0x3FFF) + 1;
 
-        return WebP::fromChunks($width, $height, [$topChunk]);
+        return WebP::fromChunks(
+            $width,
+            $height,
+            [
+                new Chunk("VP8L", $buffer->setPosition($startOfData)->getString($length))
+            ]
+        );
     }
 
-    private function decodeExtendedHeader(string $binary, int $offset): WebP
+    private function decodeExtendedHeader(Buffer $buffer): WebP
     {
         // After the `VP8X` header there are 4 more bytes that appear to have no
         // meaning but the first byte is always set to 0x0A.
-        $offset += 4;
+        $buffer->skip(4);
 
         // The following 4 bytes contain a single byte containing a bitmask for
         // the contained features (which we can safely ignore at this point),
         // followed by 24 reserved bits.
-        $offset += 4;
+        $buffer->skip(4);
 
         // The width of the canvas is represented as a uint24LE but minus one,
         // therefore we have to add 1 when decoding the value.
-        $width = $this->decodeDimension($binary, $offset);
-        $offset += 3;
+        $width = $this->decodeDimension($buffer);
 
         // The height follows the same rules as the width.
-        $height = $this->decodeDimension($binary, $offset);
-        $offset += 3;
+        $height = $this->decodeDimension($buffer);
 
         // Decode the remaining chunks.
         $chunks = [];
-        $totalSize = \strlen($binary);
-        while ($offset < $totalSize) {
-            $chunk = $this->decodeChunk($binary, $offset);
+        while ($buffer->hasRemaining()) {
+            $chunk = $this->decodeChunk($buffer);
             $chunks[] = $chunk;
 
             // RIFF requires all chunks to be of even length, chunks with an
             // uneven length must be padded by a single 0x00 at the end.
-            $paddingByte = $chunk->length % 2;
-            $offset += $chunk->length + $paddingByte;
+            if ($chunk->length % 2 === 1) {
+                $buffer->skip(1);
+            }
         }
-
-        \assert($offset === $totalSize);
 
         if ($chunks === []) {
             throw new RuntimeException("TODO: VP8X contains no data");
@@ -174,33 +179,30 @@ final class Decoder
         return WebP::fromChunks($width, $height, $chunks);
     }
 
-    private function decodeChunk(string $binary, int $offset): Chunk
+    private function decodeChunk(Buffer $buffer): Chunk
     {
-        $totalSize = \strlen($binary);
-        if ($offset + 8 > $totalSize) {
-            $remainingBytes = $totalSize - $offset;
+        $remainingBytes = $buffer->remaining();
+        if ($remainingBytes < 8) {
+            $offset = $buffer->position();
             throw new RuntimeException("Unexpected EOF, expected a chunk at offset {$offset} but there are only {$remainingBytes} more bytes");
         }
 
-        $header = \unpack('A4fourCC/Vlength', $binary, $offset);
-        \assert($header !== false);
-
-        $offset += 8;
-
-        $fourCC = $header['fourCC'];
-        $length = $header['length'];
-        if ($offset + $length > $totalSize) {
+        $fourCC = $buffer->getString(4);
+        $length = $buffer->getUnsignedInt();
+        if ($buffer->remaining() < $length) {
+            $offset = $buffer->position();
             throw new RuntimeException("TODO: length {$length} for chunk {$fourCC} at offset {$offset} is out of bounds");
         }
 
-        return new Chunk($fourCC, \substr($binary, $offset, $length));
+        return new Chunk($fourCC, $buffer->getString($length));
     }
 
-    private function decodeDimension(string $binary, int $offset): int
+    private function decodeDimension(Buffer $buffer): int
     {
-        $bytes = \unpack('ca/cb/cc', $binary, $offset);
-        \assert($bytes !== false);
+        $a = $buffer->getUnsignedByte();
+        $b = $buffer->getUnsignedByte();
+        $c = $buffer->getUnsignedByte();
 
-        return $bytes['a'] + ($bytes['b'] << 8) + ($bytes['c'] << 16) + 1;
+        return $a + ($b << 8) + ($c << 16) + 1;
     }
 }
